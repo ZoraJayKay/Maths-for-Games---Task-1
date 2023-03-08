f97a457a1dc0cedcfff332c7b8b6c4cd52996a24
ZoraJayKay <126629866+ZoraJayKay@users.noreply.github.com>
2023-03-08 16:25:29 +1100

    Create Visual Studio project
    1st commit: draw down readme and gitignore from the web, create a blank VS project for this task on the local drive and synchronise with github

---
+++ MFG Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFG_Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
